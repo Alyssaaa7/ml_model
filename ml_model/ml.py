3a4be58648e81934564c6bf43cfb8d788aefffe0
Alyssaaa7 <69889753+Alyssaaa7@users.noreply.github.com>
2023-08-02 18:29:05 -0400

    Update ml.py

--- ml_model/ml.py
+++ ml_model/ml.py
@@ -1,37 +1,54 @@
 import pandas as pd
 import numpy as np
+import random
 
-# read your csv file into a DataFrame
-df = pd.read_csv('your_file.csv')
+# Read data from CSV file
+df = pd.read_csv('timestamps.csv')
 
-# make sure 'timestamp' column is in datetime format
+# Convert timestamps to datetime and extract the time
 df['timestamp'] = pd.to_datetime(df['timestamp'])
+df['time'] = df['timestamp'].dt.time
 
-# extract weekday name, hour and minute, then create a new column for 30-minute interval 
-df['weekday'] = df['timestamp'].dt.day_name()
-df['hour'] = df['timestamp'].dt.hour
-df['minute'] = df['timestamp'].dt.minute
-df['time_interval'] = df['hour'] + np.where(df['minute'] >= 30, 0.5, 0)
+# Define the time intervals
+intervals = pd.date_range(start='00:00', end='23:59', freq='30min').time
 
-# count occurrences for each weekday and time_interval
-count_df = df.groupby(['weekday', 'time_interval']).size().reset_index(name='counts')
+# Count the occurrences in each interval
+df['interval'] = pd.cut(df['time'], bins=intervals, include_lowest=True)
+counts = df['interval'].value_counts()
 
-# calculate total counts for each weekday
-total_counts = count_df.groupby('weekday')['counts'].sum().reset_index(name='total_counts')
+# Calculate probabilities for each interval
+probs = counts / counts.sum()
 
-# merge total counts back to the count_df
-count_df = pd.merge(count_df, total_counts, on='weekday')
+# Calculate the expected number of timestamps in each interval for a total of 50 timestamps
+expected_counts = (probs * 50).round().astype(int)
 
-# calculate probability
-count_df['probability'] = count_df['counts'] / count_df['total_counts']
+# Generate the new timestamps
+new_timestamps = []
 
-# simulate for a specific day
-def simulate_day(day_name):
-    specific_day_df = count_df[count_df['weekday'] == day_name]
-    return np.random.choice(specific_day_df['time_interval'], size=50, p=specific_day_df['probability'])
+for interval, count in expected_counts.items():
+    # Calculate the start and end of the interval
+    start = interval.left
+    end = interval.right
+    
+    # Calculate the difference between the end and start times in minutes
+    diff = ((end.hour * 60 + end.minute) - (start.hour * 60 + start.minute))
+    
+    # If diff is zero, that means we are at the end of the time frame i.e. 00:00, so set diff to 24*60 minutes
+    if diff == 0:
+        diff = 24*60
 
-# simulate for a weekday
-print(simulate_day('Monday'))
+    # Calculate the spacing between each timestamp
+    if count > 0:
+        spacing = diff / count
+    else:
+        continue
 
-# simulate for a weekend
-print(simulate_day('Saturday'))
+    # Generate the timestamps
+    for i in range(count):
+        minute = start.hour * 60 + start.minute + i*spacing
+        hour = minute // 60
+        minute = minute % 60
+        new_timestamps.append(pd.Timestamp(year=2023, month=8, day=2, hour=int(hour), minute=int(minute)))
+
+new_timestamps
+
