dd5abb92fd88953fb91f2c1797cf014614a0e8e9
Alyssaaa7 <69889753+Alyssaaa7@users.noreply.github.com>
2023-08-02 19:47:58 -0400

    Update ml.py

--- ml_model/ml.py
+++ ml_model/ml.py
@@ -18,24 +18,18 @@ counts = df['interval'].value_counts(sort=False)
 # calculate the probabilities
 probabilities = counts / counts.sum()
 
-# the total number of timestamps to generate
 N = 50
 
 # calculate the expected number of timestamps in each interval
 expected_counts = (probabilities * N).round().astype(int)
 
-# calculate the actual number of timestamps to generate in each interval
-actual_counts = expected_counts.clip(upper=1)
+# calculate the total number of generated timestamps
+total_generated = expected_counts.sum()
 
-# calculate the remaining number of timestamps to generate
-remaining = N - actual_counts.sum()
-
-# distribute the remaining timestamps according to the original probabilities
-remaining_counts = np.random.choice(probabilities.index, size=remaining, p=probabilities.values)
-remaining_counts = pd.Series(remaining_counts).value_counts()
-
-# add the remaining counts to the actual counts
-actual_counts += remaining_counts
+# if the total number is not equal to N, adjust the counts proportionally
+if total_generated != N:
+    adjustment_factor = N / total_generated
+    expected_counts = (expected_counts * adjustment_factor).round().astype(int)
 
 # create a function to generate evenly spaced timestamps within an interval
 def generate_timestamps_in_interval(interval, n):
@@ -50,7 +44,7 @@ def generate_timestamps_in_interval(interval, n):
 
 # generate the timestamps
 timestamps = []
-for interval, count in actual_counts.items():
+for interval, count in expected_counts.items():
     timestamps.extend(generate_timestamps_in_interval(interval, count))
 
 # sort the timestamps
